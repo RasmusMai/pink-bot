4731bfc3bd93b81b31046d6ab66174350c94f9c4
Rasmus M <rasmusmaipuu@gmail.com>
2017-01-07 19:47:21 +0200

    Added random.cat and small changes
    Optimized a few things and patched some bugs. Could use a lot more work

--- pink.py
+++ pink.py
@@ -3,23 +3,104 @@ from discord.ext import commands
 clear = lambda: os.system('clear')
 
 description = '''Pink - Your general purpose Discord friend.
-I'm currently young and can't do many things yet.
+Born from severe crippling depression in addition to autism.
 
-The available commands for you to use are available below, using ~ prefix.'''
+I can help you with:
+• youtube <string>					Searches for a youtube video and returns the first result
+• color or colour <string>			Changes your color to a specified value.
+• 8ball								Ask the magic eight ball
+• cat/kitty/neko					Returns a picture of a cat from random.cat
+
+My commands are not (at the very least, shouldn\'t be) case sensitive.'''
 bot = commands.Bot(command_prefix='~', description=description, category='category', pm_help=True)
 
 @bot.event
 async def on_ready():
 	clear()
+	#last_message = ''
 	print('I\'m starting up! My username is {}'.format(bot.user.name))
 	print('My id - {}'.format(bot.user.id))
 	print('------')
-	await bot.change_presence(game=discord.Game(name = 'Type ~help for info'))
+	#await bot.change_presence(game=discord.Game(name = 'Use `color <colorname>`'))
 
 @bot.event
 async def on_message(message):
+	#Checks that Pink won't reply to herself
+	message_content = message.content.strip('/n').lower()
+
 	if message.author == bot.user:
-		return;
+		return
+
+	#Searches youtube for the keyword and returns the first result
+	if message_content.startswith("youtube "):
+		query_string = urllib.parse.urlencode({"search_query" : message.content.split(' ', 1)[1]})
+		html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+		search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+		first_result = "http://www.youtube.com/watch?v=" + search_results[0]
+		await bot.send_message(message.channel,first_result)
+		return
+	if message_content == 'cat' or message_content == 'kitty' or message_content == 'neko':
+		await bot.send_typing(message.channel)
+		try:
+			html_content = urllib.request.urlopen("http://random.cat/meow")
+			html_content = html_content.read().decode()[:-2]
+			picture_to_download = re.search(r"([-\w]+\.(jpg|png|gif|bmp))", html_content)
+			picture_to_download = picture_to_download.group(1)
+			urllib.request.urlretrieve("http://random.cat/i/"+picture_to_download,"temporary/"+picture_to_download)
+			await bot.send_file(message.channel, 'temporary/'+picture_to_download)
+		except:
+			await bot.send_message(message.channel, "Sorry, I couldn't do it.")
+
+	#People with permissions can change the presence of Pink
+	if message_content.startswith("changepresence ") and allowed(message.author.id):
+		print ('changing presence to '+ message_content.split(' ', 1)[1])
+		await bot.change_presence(game=discord.Game(name = message_content.split(' ', 1)[1]))
+		return
+
+	#Deletes the last message sent by Pink.py
+	if message_content.startswith('deletelast') or message_content == 'deletelast':
+		if allowed(message.author.id):
+			pass
+
+	#A meme command, no use
+	if message_content.startswith('8ball') or message_content == '8ball':
+		await bot.send_typing(message.channel)
+		await asyncio.sleep(3)
+		eightball_array = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'The stars say no', 'Outlook not so good', 'Very doubtful']
+		await  bot.send_message(message.channel, random.choice(eightball_array))
+		return
+
+	#Self color changing
+	#First, it checks if the color requested is in the list of possible colors.
+	#Then it finds the server's equivalent for that color. It finds and matches the server role by name.
+	#That automates the process and doesn't require storing of the server roles.
+	#Lastly, it removes all other color roles from the requester and gives them the new one.
+	if message_content.startswith("color ") or message_content.startswith("colour "):
+		possible_colors = ['red','blue','green','purple','orange','yellow','grey','brown','none']
+		requested_color = message.content.split(' ', 1)[1]
+		requested_color = requested_color.lower().strip()
+		if requested_color in possible_colors:
+			for server_role in message.server.roles:
+				if server_role.name.lower() == requested_color:
+					role_given = server_role
+			for color in possible_colors:
+				for current_role in message.author.roles:
+					if current_role.name.lower() in possible_colors:
+						if requested_color == current_role.name.lower():
+							await bot.send_message(message.channel, "You already have that color")
+							return
+						await bot.remove_roles(message.author, current_role)
+			if requested_color == 'none':
+				await bot.send_message(message.channel, "You are now without a color. Doesn\'t it feel great to be different!")
+			else:
+				await bot.add_roles(message.author, role_given)
+				await bot.send_message(message.channel,"Your color is now "+ requested_color)
+			print ("Changing the color of " + message.author.name + " to " + requested_color)
+		else:
+			await bot.send_message(message.channel,"I can only give you one of the following colors: red, blue, green, purple, orange, yellow, grey and brown.")
+		return
+
+	#Scans the message for trigger words
 	msg_array = message.content.split()
 	for word in msg_array:
 		if word.lower() == "weeb" or word.lower() == "weeaboo":
@@ -37,43 +118,10 @@ async def on_message(message):
 			else:
 				await bot.send_message(message.channel,msg_to_be_sent)
 			return
-	if message.content.startswith("changepresence ") and allowed(message.author.id):
-		print ('changing presence to '+ message.content.split(' ', 1)[1])
-		await bot.change_presence(game=discord.Game(name = message.content.split(' ', 1)[1]))
-		return
-	if message.content.startswith("youtube "):
-		query_string = urllib.parse.urlencode({"search_query" : message.content.split(' ', 1)[1]})
-		html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-		search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-		first_result = "http://www.youtube.com/watch?v=" + search_results[0]
-		await bot.send_message(message.channel,first_result)
-		return
-	if message.content.startswith('8ball') or message.content == '8ball':
-		await bot.send_typing(message.channel)
-		await asyncio.sleep(3)
-		eightball_array = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'The stars say no', 'Outlook not so good', 'Very doubtful']
-		await  bot.send_message(message.channel, "The Magic 8-Ball has decided - " + random.choice(eightball_array))
-	if message.content.startswith("color ") or message.content.startswith("colour "):
-		possible_colors = ['red','blue','green','purple','orange','yellow','grey','brown']
-		requested_color = message.content.split(' ', 1)[1]
-		requested_color.lower()
-		requested_color.strip()
-		if requested_color in possible_colors:
-			for server_role in message.server.roles:
-				if server_role.name == requested_color:
-					role_given = server_role
-			for color in possible_colors:
-				for current_role in message.author.roles:
-					if current_role.name in possible_colors:
-						await bot.remove_roles(message.author, current_role)
-				if color in message.author.roles:
-					pass
-					#await bot.remove_roles(message.author,"nein")
-			await bot.add_roles(message.author, role_given)
-		else:
-			await bot.send_message(message.channel,"I can only edit the following colors: red, blue, green, purple, orange, yellow, grey and brown.")
 	await bot.process_commands(message)
+	return
 
+#Reacts to and announces member joining or leaving
 @bot.event
 async def on_member_join(Member):
 	await bot.send_message(Member.server, Member.mention + " has joined. Please state your name and purpose.")
@@ -81,6 +129,7 @@ async def on_member_join(Member):
 async def on_member_remove(Member):
 	await bot.send_message(Member.server, Member.mention + " has left :o")
 
+#If called, it checks if the message.author has permissions. The list of people with administrative rights are stored in allowed_users.dat as id
 def allowed(msg_id):
 	allowed_file = "allowed_users.dat";
 	allowed_array = []
@@ -90,28 +139,11 @@ def allowed(msg_id):
 				return True
 	return False
 
+#265442795446075397
+#https://discordapp.com/oauth2/authorize?client_id=265442795446075397&scope=bot&permissions=0
 parser = argparse.ArgumentParser()
 parser.add_argument("-t","--token")
 args = parser.parse_args()
 token = args.token
-'''
-try:
-	auth = "auth.txt"
-	autharray = []
-	with open(auth, "r") as file:
-		for value in file.read().split(':'):
-			autharray.append(value)
-	if len(autharray) > 0:
-		token = autharray[0]
-	#pswd = autharray[1]
-	file.close()
-except FileNotFoundError:
-	print ('---No auth.txt detected or empty---')
-	token = input('Token ==> ')
-
-        #pswd = getpass.getpass('Password ==> ')
-'''
-#265442795446075397
-#https://discordapp.com/oauth2/authorize?client_id=265442795446075397&scope=bot&permissions=0
 bot.login('token')
 bot.run(token)
