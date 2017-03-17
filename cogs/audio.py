e21249db13ffbb1ec2a19ff7ecf85018256b6a3a
Rasmus M <rasmusmaipuu@gmail.com>
2017-03-17 18:53:57 +0200

    Meme bugs

--- cogs/audio.py
+++ cogs/audio.py
@@ -9,20 +9,18 @@ class Audio:
 
     def __init__(self, bot):
         self.bot = bot
-        self.voice = None
-        self.player = None
 
     @commands.command(pass_context=True)
     async def joinvoice(self, ctx):
         if ctx.message.author.voice.voice_channel is not None:
-            self.voice = await bot.join_voice_channel()
+            self.voice = await self.bot.join_voice_channel()
             await self.bot.say("Joined voice channel "+self.voice.channel.name)
         else:
             await self.bot.say("Please join a voice channel and request again. I can't follow you to nowhere.")
 
     @commands.command(pass_context=True)
     async def playyoutube(self, ctx, target : str):
-        if voice_state_for(self.bot) is None:
+        if voice.is_connected() is None:
             if ctx.message.author.voice.voice_channel is not None:
                 self.voice = await bot.join_voice_channel()
                 await self.bot.say("Joined voice channel "+self.voice.channel.name)
@@ -43,7 +41,7 @@ class Audio:
 
     @commands.command()
     async def stopvoice(self, ctx):
-        if voice_state_for(self.bot) is None:
+        if voice.is_connected() is None:
             await self.bot.say("I'm not in any voice channels.")
         if self.player.is_playing():
             self.player.stop()
